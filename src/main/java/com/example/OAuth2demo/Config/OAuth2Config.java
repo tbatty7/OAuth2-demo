ace80992173222afc0be7f5ac3ecf2eae0674605
Tim B <tbatty7@gmail.com>
2019-01-04 22:31:37 -0500

    added role

--- src/main/java/com/example/OAuth2demo/Config/OAuth2Config.java
+++ src/main/java/com/example/OAuth2demo/Config/OAuth2Config.java
@@ -28,6 +28,8 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
                 .withClient("service-account-1")
                 .secret(encoder.encode("service-account-1-secret"))
                 .authorizedGrantTypes("client_credentials")
-                .scopes("resource-server-read", "resource-server-write");
+                .scopes("resource-server-read", "resource-server-write")
+                .authorities("ROLE_RS_READ");
+
     }
 }
